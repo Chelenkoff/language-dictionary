1df7430abb2ab80b05d3c9548c2eaae23985b55b
Chelenkoff <resiloveca94@gmail.com>
2016-03-07 14:13:28 +0200

    Languages class added MVC structured

---
+++ language_dictionary/Model/Language.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace language_dictionary
+{
+    class Language
+    {
+        private string name;
+        private string descriptor;
+
+        //Constructor
+        Language(string name, string descriptor)
+        {
+            this.name = name;
+            this.descriptor = descriptor;
+        }
+
+        //'Name' getter
+        public string getName()
+        {
+            return name;
+        }
+        //'Descriptor' getter
+        public string getDescriptor()
+        {
+            return descriptor;
+        }
+    }
+}
